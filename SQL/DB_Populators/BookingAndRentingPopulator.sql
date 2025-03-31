77f45f777466e7fbd5dae16c1af6ee66e1316dc5
Samuel Krutis <samuel.krutis@gmail.com>
2025-03-31 17:28:35 -0400

    Added Triggers for Booking and Renting to Archive

--- SQL/DB_Populators/BookingAndRentingPopulator.sql
+++ SQL/DB_Populators/BookingAndRentingPopulator.sql
@@ -1,3 +1,4 @@
+-- @block
 INSERT INTO booking(room_id, customer_id, booking_date, checkin_date, checkout_date, status) VALUES
 (2, 1, '2025-03-24', '2025-04-10', '2025-04-15', 'CONFRIMED'),
 (8, 2, '2025-03-25', '2025-05-01', '2025-05-05', 'CONFIRMED'),
@@ -10,3 +11,7 @@ INSERT INTO renting(room_id, customer_id, start_date, end_date, payment_status) 
 (1, 1, '2025-03-31', '2025-04-04', 'Pending'),
 (9, 2, '2025-03-31', '2025-04-05', 'Pending'),
 (15, 3, '2025-03-31', '2025-04-06', 'Pending');
+
+-- @block
+INSERT INTO booking(room_id, customer_id, booking_date, checkin_date, checkout_date, status) VALUES
+(2, 1, '2025-03-24', '2025-04-10', '2025-04-15', 'CONFRIMED');
